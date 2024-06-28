49f80341a43234e2e6689ae22f10fea5e23e152f
Doa'a Osama Adill Khalaf <doaaosamakhalaf@gmail.com>
2024-06-28 10:03:51 -0700

    Theme Consts Done

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,13 @@ const messageNotification = document.querySelector('#messages-notifications');
 const messages = document.querySelector('.messages');
 const message = messages.querySelectorAll('.message');
 const messageSearch = document.querySelector('#message-search');
+
+//Theme
+const theme = document.querySelector('#theme');
+const themeModal = document.querySelector('.customize-theme');
+const fontSize = document.querySelectorAll('.choose-size span');
+var root = document.querySelector(':root');
+const colorPalette = document.querySelectorAll('.choose-color span');
+const Bg1 = document.querySelector('.bg-1');
+const Bg2 = document.querySelector('.bg-2');
+const Bg3 = document.querySelector('.bg-3');
