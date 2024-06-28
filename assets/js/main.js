a71a2ce7b91cc28bce535eb0871619c75729c721
Doa'a Osama Adill Khalaf <doaaosamakhalaf@gmail.com>
2024-06-28 10:03:28 -0700

    Messages Consts Done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,8 @@
 // Sidebar
 const menuItems = document.querySelectorAll('.menu-item');
+
+// Messages 
+const messageNotification = document.querySelector('#messages-notifications');
+const messages = document.querySelector('.messages');
+const message = messages.querySelectorAll('.message');
+const messageSearch = document.querySelector('#message-search');
